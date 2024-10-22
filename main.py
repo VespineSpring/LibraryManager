1fffb9991e2a805ae8463681f1c964bc91ff2481
Vespine <142816007+VespineSpring@users.noreply.github.com>
2024-10-22 22:44:35 +0530

    Created Tables

--- main.py
+++ main.py
@@ -1,9 +1,75 @@
-import mysql.connector
+import mysql.connector as sqltor
 
-connection = mysql.connector.connect(host="localhost", user="root", password="root")
+
+def create_database_and_table():
+    connection = sqltor.connect(host="localhost", user="root", password="root")
+
+    if connection.is_connected():
+        cursor = connection.cursor()
+
+        cursor.execute("CREATE DATABASE IF NOT EXISTS librarymanager")
+        cursor.execute("USE librarymanager")
+
+        cursor.execute(
+            """CREATE TABLE IF NOT EXISTS users (
+                user_id INT AUTO_INCREMENT PRIMARY KEY,
+                name VARCHAR(100) NOT NULL,
+                phone_number VARCHAR(10) NOT NULL
+            )
+            """
+        )
+
+        cursor.execute(
+            """CREATE TABLE IF NOT EXISTS books (
+                book_id INT AUTO_INCREMENT PRIMARY KEY,
+                title VARCHAR(100) NOT NULL,
+                author VARCHAR(100) NOT NULL,
+                available INT DEFAULT 1
+            )
+            """
+        )
+
+        cursor.execute(
+            """CREATE TABLE IF NOT EXISTS rented_books (
+                rental_id INT AUTO_INCREMENT PRIMARY KEY,
+                user_id INT,
+                book_id INT,
+                rental_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+                FOREIGN KEY (user_id) REFERENCES users(user_id),
+                FOREIGN KEY (book_id) REFERENCES books(book_id)
+            )
+            """
+        )
+
+        cursor.execute(
+            """CREATE TABLE IF NOT EXISTS returned_books (
+                return_id INT AUTO_INCREMENT PRIMARY KEY,
+                rental_id INT,
+                return_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+                FOREIGN KEY (rental_id) REFERENCES rented_books(rental_id)
+            )
+            """
+        )
+    else:
+        print("Error while connecting the database.")
+        return
+
+    cursor.close()
+    connection.close()
+
+
+create_database_and_table()
+
+connection = sqltor.connect(
+    host="localhost", user="root", password="root", database="librarymanager"
+)
 cursor = connection.cursor()
 
-cursor.execute("create database if not exists medicalclinic")
-cursor.execute("use medicalclinic")
+cursor.execute("SHOW TABLES")
+tables = cursor.fetchall()
 
-print(connection.database)
+for table in tables:
+    print(table[0])
+
+cursor.close()
+connection.close()
